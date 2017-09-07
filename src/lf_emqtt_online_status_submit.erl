e79c68890bb6601d5f91cda341a4f078d1df5794
sasiNiranjana <sasitha.agampodi@lifefitness.com>
2017-09-07 13:17:01 +0700

    Removed string equal method and added manual checking

--- src/lf_emqtt_online_status_submit.erl
+++ src/lf_emqtt_online_status_submit.erl
@@ -20,8 +20,6 @@
 
 -export([load/1, unload/0]).
 
--import(string,[equal/3]).
-
 %% Hooks functions
 
 -export([on_client_connected/3, on_client_disconnected/3]).
@@ -32,9 +30,9 @@ load(Env) ->
     emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3, [Env]).
 
 on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) ->
-    A = binary_part(ClientId,{0,6}), B = not equal(A,"nimbus",true),
+    A = binary_part(ClientId,{0,6}), B = <<"Nimbus">>, C = <<"nimbus">>, D = <<"NIMBUS">>, E = A/=B, F = A/=C, G = A/=D, H = E and F,
     if
-	B ->
+	H and G ->
 	    Server=proplists:get_value(server,_Env,"http://localhost:8080/lfservices/api/asset_online_status/emqtt_update_status"),
 	    ContentType="application/json",
 	    Message = "{\"bodySerial\":\"" ++ binary_to_list(ClientId) ++ "\",\"status\":true}",
@@ -47,9 +45,9 @@ on_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) 
 	
 
 on_client_disconnected(Reason, _Client = #mqtt_client{client_id = ClientId}, _Env) ->
-    A = binary_part(ClientId,{0,6}), B = not equal(A,"nimbus",true),
+    A = binary_part(ClientId,{0,6}), B = <<"Nimbus">>, C = <<"nimbus">>, D = <<"NIMBUS">>, E = A/=B, F = A/=C, G = A/=D, H = E and F,
     if
-	B ->
+	H and G ->
 	    Server=proplists:get_value(server,_Env,"http://localhost:8080/lfservices/api/asset_online_status/emqtt_update_status"),
 	    ContentType="application/json",
 	    Message = "{\"bodySerial\":\"" ++ binary_to_list(ClientId) ++ "\",\"status\":false}",
